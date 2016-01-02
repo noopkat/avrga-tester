325cb7ba859e2058f587d347b4e90928762d87ce
Suz Hinton <suz.hinton@gmail.com>
2016-01-02 01:01:07 -0500

    used injected Avrgirl dependency for startup

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var Avrgirl = require('avrgirl-arduino');
-var express = require('express');  
+var express = require('express');
 var path = require('path');
 var app = express();
 var expressWs = require('express-ws')(app);
@@ -7,67 +6,63 @@ var opener = require('opener');
 var os = require('os');
 var fs = require('fs');
 
-var avrgatv;
-var avrgav;
+module.exports = function (Avrgirl, version) {
 
-fs.readFile(path.join(__dirname, 'package.json'), function (err, data) {
+  var avrgatv;
+  var avrgav = version;
+
+  var data = fs.readFileSync(path.join(__dirname, 'package.json'), {encoding: 'utf-8'});
   var pjson = JSON.parse(data);
   avrgatv = pjson.version;
-});
 
-fs.readFile(path.join(__dirname, 'node_modules', 'avrgirl-arduino', 'package.json'), function (err, data) {
-  var pjson = JSON.parse(data);
-  avrgav = pjson.version;
-});
+  var port = process.env.PORT || 7000;
 
-var port = process.env.PORT || 7000;
+  app.use('/assets', express.static(__dirname + '/frontend/assets/'));
 
-app.use('/assets', express.static(__dirname + '/frontend/assets/'));
-
-app.get('/*', function(req, res) {
-  res.sendFile(__dirname + '/frontend/index.html');
-});
-
-app.ws('/', function(ws, req) {
-  ws.send(JSON.stringify({type: 'message', body: 'Hey! Websockets are working.'}));
-  ws.on('message', function(message) {
-    messageHandler(message, ws);
+  app.get('/*', function(req, res) {
+    res.sendFile(__dirname + '/frontend/index.html');
   });
-});
 
-var server = app.listen(port, 'localhost', setUpApp);
-
-function setUpApp() {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Now running AVRGA tester on http://%s:%s', host, port);
-  opener('http://' + host + ':' + port);
-}
-
-function messageHandler(message, ws) {
-  console.log('received: %s', message);
-
-  var blink = path.join(__dirname, 'node_modules', 'avrgirl-arduino', 'junk', 'hex', message, 'Blink.cpp.hex');
-
-  var avrgirl = new Avrgirl({board: message});
-
-  avrgirl.flash(blink, function(err) {
-    console.log(err, 'done flashing!');
-    if (err) {
-      err = err.message;
-    }
-
-    var report = {
-      type: 'report',
-      error: err,
-      nodev: process.version.slice(1),
-      os: os.platform(),
-      osv: os.release(),
-      avrgatv: avrgatv,
-      avrgav: avrgav,
-      board: message
-    }
-    ws.send(JSON.stringify(report));
+  app.ws('/', function(ws, req) {
+    ws.send(JSON.stringify({type: 'message', body: 'Hey! Websockets are working.'}));
+    ws.on('message', function(message) {
+      messageHandler(message, ws);
+    });
   });
-}
+
+  var server = app.listen(port, 'localhost', setUpApp);
+
+  function setUpApp() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Now running AVRGA tester on http://%s:%s', host, port);
+    opener('http://' + host + ':' + port);
+  }
+
+  function messageHandler(message, ws) {
+
+    var blink = path.join(__dirname, 'node_modules', 'avrgirl-arduino', 'junk', 'hex', message, 'Blink.cpp.hex');
+
+    var avrgirl = new Avrgirl({board: message});
+
+    avrgirl.flash(blink, function(err) {
+      console.log(err, 'done flashing!');
+      if (err) {
+        err = err.message;
+      }
+
+      var report = {
+        type: 'report',
+        error: err,
+        nodev: process.version.slice(1),
+        os: os.platform(),
+        osv: os.release(),
+        avrgatv: avrgatv,
+        avrgav: avrgav,
+        board: message
+      }
+      ws.send(JSON.stringify(report));
+    });
+  }
+};
---
+++ test-pilot.js
@@ -0,0 +1,13 @@
+var testpilot = require('./index');
+var Avrgirl = require('avrgirl-arduino');
+var fs = require('fs');
+var path = require('path');
+
+var avrgav;
+
+fs.readFile(path.join(__dirname, 'node_modules', 'avrgirl-arduino', 'package.json'), function (err, data) {
+  var pjson = JSON.parse(data);
+  avrgav = pjson.version;
+  testpilot(Avrgirl, avrgav);
+});
+
