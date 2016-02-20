9075b690d45408d86e4a44253f985e202d366bc6
Suz Hinton <suz.hinton@gmail.com>
2016-02-19 21:08:54 -0500

    use route model for report summary

--- ember-app/app/components/report-summary.js
+++ ember-app/app/components/report-summary.js
@@ -1,11 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  reportService: Ember.inject.service('report-service'),
-  report: {},
-  init: function() {
-    this._super.apply(this, arguments);
-    // get report from service
-    this.set('report', this.get('reportService').report);
-  }
 });
--- ember-app/app/routes/report.js
+++ ember-app/app/routes/report.js
@@ -1,9 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  reportService: Ember.inject.service('report-service'),
   actions: {
     gotoThanks: function() {
       this.transitionTo('thanks');
     }
+  },
+  model() {
+    return this.get('reportService.report');
   }
 });
